3ef88a718234573713b4f1f7fab04d110e325047
thenightterorx <thenightterrorx@gmail.com>
2023-05-25 05:44:43 -0400

    refined node.js

--- altremote.js
+++ altremote.js
@@ -1,11 +1,25 @@
 const http = require("http");
+const { exec } = require('node:child_process');
+const fs = require('fs').promises;
 const host = '0.0.0.0';
 const port = 8000;
 
+exec("mkdir /home/lee/hello");
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end("My first server!");
+  //res.writeHead(200);
+  
+  res.setHeader("Content-Type", "application/x-7z-compressed");
+  console.log(req.url);
+  exec("mkdir /home/"+process.env.USER+"/servercache/"+req.url);
+  exec("wget -P /home/lee/servercache/"+req.url+" "+req.url);
+  exec("7z a /home/"+process.env.USER+"/servercache/"+req.url+".7z /home/"+process.env.USER+"/servercache/"+req.url );
+
+  fs.readFile(filePath, function(error, content) {
+    response.writeHead(200, { 'Content-Type': "application/x-7z-compressed"});
+    response.end(content, 'utf-8');
+  }
+
 };
 
 
