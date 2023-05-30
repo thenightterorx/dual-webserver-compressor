3d3c705bee7f73364eaae7f145215b7b342c11ec
thenightterorx <thenightterrorx@gmail.com>
2023-05-30 05:22:10 -0400

    hopefullyfixes

---
+++ altlocalwithtemp.js
@@ -0,0 +1,121 @@
+const http = require("http");
+const { exec } = require('node:child_process');
+const execSync = require('child_process').execSync;
+const fs = require('fs').promises;
+const host = '0.0.0.0';
+const port = 8002;
+const shelljs = require('shelljs');
+
+var serverip = "localhost:8000";
+
+const requestListener = function (req, res) {
+  //res.writeHead(200);
+  
+  
+  console.log(req.url);
+
+  rawanted=req.url;
+  wanted=rawanted.slice(1);
+  oldwanted=rawanted.slice(1);
+  
+  if(wanted.charAt(wanted.length-1)=="/"){
+    wanted=wanted+"index.html";
+  }
+
+  wantedlist=wanted.split("/");
+  
+
+  try{
+    //mkdirerr = execSync("mkdir /home/"+process.env.USER+"/localcache/"+arguments[0]);
+    directory = "/home/"+process.env.USER+"/localcache/"+wanted;
+    createcommand= ("mkdir -p \"$(dirname \"" + directory + "\")\" && touch \"" + directory + "\"");
+    console.log(createcommand);
+    execSync(createcommand);
+
+  }catch(error){
+    console.log("cant create directory");  
+    
+    
+  }
+
+  //rmerr = execSync("rm -rf /home/"+process.env.USER+"/localcache/"+wanted);
+  //mkdirerr = execSync("mkdir /home/"+process.env.USER+"/localcache/"+wanted);
+  
+  console.log("checker a");
+
+  try{
+    console.log("checker b");
+    filePath="/home/"+process.env.USER+"/localcache/"+wanted;
+    var teststuff=execSync("cat "+filepath);
+    fs.readFile(filePath).then(contents => {
+      console.log("sending: "+filePath);
+      
+      res.writeHead(200);
+      res.end(contents);
+    })
+
+  }catch(error){
+    console.log("checker c");
+    try{
+      console.log("checker d");
+      var command=("wget -O /home/"+process.env.USER+"/localcache/"+wanted+".7z "+serverip+"/"+oldwanted);
+      console.log(command);
+      wgeterr = execSync(command);
+      
+      console.log("checker x");
+
+      decommand=("7z x /home/"+process.env.USER+"/localcache/"+wanted+".7z -o"+"/home/"+process.env.USER+"/localtemp/"+wanted); 
+      console.log(decommand);
+      decompress=execSync(decommand);
+
+      //move from temp folder to cache
+      shelljs.mv("/home/"+process.env.USER+"/localtemp/"+wanted+'/'+wanted, "/home/"+process.env.USER+"/localcache/"+wanted);
+
+      console.log("checker y");
+      filePath="/home/"+process.env.USER+"/localcache/"+wanted;
+      console.log(filePath);
+      try{
+        execSync("cat "+filepath);
+      }catch (e){
+        console.log("amogus"); 
+        filePath="/home/"+process.env.USER+"/localcache/"+wanted;
+        
+        
+       
+      }
+      fs.readFile(filePath).then(contents => {
+      console.log("sending: "+filePath);
+      
+      res.writeHead(200);
+      res.end(contents);
+      console.log("checker z");
+    })
+
+
+    }catch(err){
+      console.log("checker e");
+      res.writeHead(404);
+    }
+  }
+  //sziperror = execSync("7z a /home/"+process.env.USER+"/localcache/"+wanted+".7z /home/"+process.env.USER+"/localcache/"+wanted );
+  //ilePath="/home/"+process.env.USER+"/localcache/"+wanted+".7z"
+  //console.log(filePath);
+  /*
+  fs.readFile(filePath, function(error, content) {
+    console.log("A");
+    res.writeHead(200, { 'Content-Type': "application/x-7z-compressed"});
+    console.log(filePath);
+    res.end(content, 'utf-8');
+  })
+  */
+    
+  
+
+};
+
+
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ altremotewithtemp.js
@@ -0,0 +1,101 @@
+const http = require("http");
+const { exec } = require('node:child_process');
+const execSync = require('child_process').execSync;
+const fs = require('fs').promises;
+const host = '0.0.0.0';
+const port = 8000;
+const shelljs = require('shelljs');
+
+exec("mkdir /home/lee/hello");
+
+const requestListener = function (req, res) {
+  //res.writeHead(200);
+  
+  res.setHeader("Content-Type", "application/x-7z-compressed");
+  console.log(req.url);
+  rawanted=req.url;
+  rawoldwanted=req.url;
+
+  //idk why but these have to be separate or it breaks
+  wanted=rawanted.slice(1);
+  oldwanted=rawoldwanted.slice(1);
+
+  if (wanted.charAt(wanted.length-1)=="/"){
+    wanted=wanted+"index.html"
+  }
+  try{
+    //mkdirerr = execSync("mkdir /home/"+process.env.USER+"/servercache/"+wanted);
+    //execSync("sed -i \'s/https:\/\//localhost:8000/g\' "+filePath)
+
+    directory = "/home/"+process.env.USER+"/servercache/"+wanted;
+    createcommand= ("mkdir -p \"$(dirname \"" + directory + "\")\" && touch \"" + directory + "\"");
+    console.log(createcommand);
+    execSync(createcommand);
+
+  }catch(error){
+    rmerr = execSync("rm -rf /home/"+process.env.USER+"/servercache/"+wanted);
+    //rmerr = execSync("rm -rf /home/"+process.env.USER+"/servercache/"+)
+    rmerr = execSync("rm -rf /home/"+process.env.USER+"/servercache/"+wanted+".7z");
+  }
+
+  //rmerr = execSync("rm -rf /home/+process.env.USER+"/servercache/"+wanted);
+  //mkdirerr = execSync("mkdir /home/"+process.env.USER+"/servercache/"+wanted);
+  
+  try{
+    console.log(oldwanted);
+    wgeterr = execSync("wget -O /home/"+process.env.USER+"/servercache/"+wanted+" "+oldwanted);
+  }catch(error){
+    console.log("wget does not work");
+  }
+
+  var fixedwanted=wanted.replace("/","\\/")
+  
+  //this is a nightmare
+  //execSync("sed -i \'s/https:\\/\\//localhost:8000/g\' "               +    "/home/"+process.env.USER+"/servercache/"+wanted);
+  //execSync("sed -i \'s/href=\"\//localhost:8000\/"+fixedwanted+"/g\' "  +    "/home/"+process.env.USER+"/servercache/"+wanted);
+
+  wantedlist=wanted.split("/");
+
+  prevpath="";
+
+  for(let i=0;i<wantedlist.length-1;i++){
+    prevpath=prevpath+wantedlist[i]+"/"
+  }
+
+  try{
+    shelljs.sed('-i', 'https://',"localhost:8002/", "/home/"+process.env.USER+"/servercache/"+wanted);
+    shelljs.sed('-i', "href=\"","href=\"/"+prevpath, "/home/"+process.env.USER+"/servercache/"+wanted);
+  }catch(e){
+    console.log("/home/"+process.env.USER+"/servercache/"+wanted+" probably doesnt exist");
+  }
+  zipcommand=("7z a /home/"+process.env.USER+"/servercache/"+wanted+".7z /home/"+process.env.USER+"/servercache/"+wanted );
+  console.log(zipcommand);
+  sziperror = execSync(zipcommand);
+  filePath="/home/"+process.env.USER+"/servercache/"+wanted+".7z"
+  console.log(filePath);
+  /*
+  fs.readFile(filePath, function(error, content) {
+    console.log("A");
+    res.writeHead(200, { 'Content-Type': "application/x-7z-compressed"});
+    console.log(filePath);
+    res.end(content, 'utf-8');
+  })
+  */
+  //execSync("sed -i \'s/https:\\/\\//localhost:8000/g\' "+filePath);
+  //execSync("sed -i \'href=\"\//localhost:8000/"+wanted+"/g\' "+filePath)
+
+  fs.readFile(filePath).then(contents => {
+    console.log("sending: "+filePath);
+    res.setHeader("Content-Type", "application/x-7z-compressed");
+    res.writeHead(200);
+    res.end(contents);
+  })
+
+};
+
+
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
